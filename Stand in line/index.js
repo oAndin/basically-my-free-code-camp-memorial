a0b83cf8f8e9ea60c70dabf3a93e60e4c2aeda30
oAndin <00andherson@gmail.com>
2023-05-31 23:16:56 -0300

    Started from the exercise 56, Stand in Line

---
+++ Stand in line/index.js
@@ -0,0 +1,21 @@
+function nextInLine(arr, item) {
+    // Only change code below this line
+    // 1º Push the item with the .push() function(which will be assign when we call the function down below)
+    arr.push(item);
+    // This part i didnt got yet, but im assuming stuff
+    // since we need to return the value which was removed from our array, i must assign that value from the function  .shift() to a variable.
+    const removed = arr.shift();
+  
+    // For last, return the value assigned to the variable
+    return removed; 
+    // Only change code above this line
+  }
+  
+  // Setup bs
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code other bs
+  console.log("Before:" + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After:" + JSON.stringify(testArr));
+  
